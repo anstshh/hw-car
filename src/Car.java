e417c7dc443bd1f5095bd074a11671ca91d3a242
Admin <anstdor@gmail.com>
2022-10-10 17:17:51 +0300

    Some more changes

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,10 @@
 public class Car {
+
+    public String brand;
+    public String model;
+    public double volumeOfEngine;
+    public String colour;
+    public int yearOfProduction;
+    public String countryOfOrigin;
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car granta = new Car();
+        granta.brand = "Lada";
+        granta.model = "Granta";
+        granta.volumeOfEngine = 1.7;
+        granta.colour = "желтый";
+        granta.yearOfProduction = 2015;
+        granta.countryOfOrigin = "Россия";
+        System.out.println("Марка автомобиля " + granta.brand +
+                ", модель автомобиля " + granta.model +
+                ", объем двигателя " + granta.volumeOfEngine +
+                ", цвет автомобиля " + granta.colour +
+                ", год выпуска " + granta.yearOfProduction +
+                ", страна производства " + granta.countryOfOrigin);
+
     }
 }
