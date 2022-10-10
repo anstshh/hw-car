231e27238387604644af2e5132c3d6dccf0d171f
Admin <anstdor@gmail.com>
2022-10-10 18:05:16 +0300

    Constructor is ised

--- src/Car.java
+++ src/Car.java
@@ -7,4 +7,12 @@ public class Car {
     public int yearOfProduction;
     public String countryOfOrigin;
 
+    public Car(String brand, String model, double volumeOfEngine, String colour, int yearOfProduction, String countryOfOrigin) {
+        this.brand = brand;
+        this.model = model;
+        this.volumeOfEngine = volumeOfEngine;
+        this.colour = colour;
+        this.yearOfProduction = yearOfProduction;
+        this.countryOfOrigin = countryOfOrigin;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car granta = new Car();
-        granta.brand = "Lada";
-        granta.model = "Granta";
-        granta.volumeOfEngine = 1.7;
-        granta.colour = "желтый";
-        granta.yearOfProduction = 2015;
-        granta.countryOfOrigin = "Россия";
+        Car granta = new Car("Lada","Granta",1.7,"желтый",2015, "Россия");
         System.out.println("Марка автомобиля " + granta.brand +
                 ", модель автомобиля " + granta.model +
                 ", объем двигателя " + granta.volumeOfEngine +
@@ -15,13 +9,8 @@ public class Main {
                 ", год выпуска " + granta.yearOfProduction +
                 ", страна производства " + granta.countryOfOrigin);
 
-        Car audi = new Car();
-        audi.brand = "Audi";
-        audi.model = "A8 50 L TDI quattro";
-        audi.volumeOfEngine = 3.0;
-        audi.colour = "черный";
-        audi.yearOfProduction =  2020;
-        audi.countryOfOrigin = "Германия";
+        Car audi = new Car("Audi","A8 50 L TDI quattro",3.0,"черный",2020,"Германия");
+
         System.out.println("Марка автомобиля " + audi.brand +
                 ", модель автомобиля " + audi.model +
                 ", объем двигателя " + audi.volumeOfEngine +
@@ -29,13 +18,8 @@ public class Main {
                 ", год выпуска " + audi.yearOfProduction +
                 ", страна производства " + audi.countryOfOrigin);
 
-        Car bmw = new Car();
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.volumeOfEngine = 3.0;
-        bmw.colour = "черный";
-        bmw.yearOfProduction =  2021;
-        bmw.countryOfOrigin = "Германия";
+        Car bmw = new Car("BMW","Z8",3.0,"черный",2021,"Германия");
+
         System.out.println("Марка автомобиля " + bmw.brand +
                 ", модель автомобиля " + bmw.model +
                 ", объем двигателя " + bmw.volumeOfEngine +
@@ -43,13 +27,8 @@ public class Main {
                 ", год выпуска " + bmw.yearOfProduction +
                 ", страна производства " + bmw.countryOfOrigin);
 
-        Car kia = new Car();
-        kia.brand = "Kia";
-        kia.model = "Sportage 4 поколение";
-        kia.volumeOfEngine = 2.4;
-        kia.colour = "красный";
-        kia.yearOfProduction =  2018;
-        kia.countryOfOrigin = "Южная Корея";
+        Car kia = new Car("Kia","Sportage 4 поколение",2.4,"красный",2018,"Южная Корея");
+
         System.out.println("Марка автомобиля " + kia.brand +
                 ", модель автомобиля " + kia.model +
                 ", объем двигателя " + kia.volumeOfEngine +
@@ -57,13 +36,8 @@ public class Main {
                 ", год выпуска " + kia.yearOfProduction +
                 ", страна производства " + kia.countryOfOrigin);
 
-        Car hyundai = new Car();
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.volumeOfEngine = 1.6;
-        hyundai.colour = "оранжевый";
-        hyundai.yearOfProduction =  2016;
-        hyundai.countryOfOrigin = "Южная Корея";
+        Car hyundai = new Car("Hyundai","Avante",1.6,"оранжевый",2016,"Южная Корея");
+
         System.out.println("Марка автомобиля " + hyundai.brand +
                 ", модель автомобиля " + hyundai.model +
                 ", объем двигателя " + hyundai.volumeOfEngine +
