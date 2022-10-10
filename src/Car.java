be9b5f0016d3deb30ce51d0e558c739b9a3e433c
Admin <anstdor@gmail.com>
2022-10-10 18:21:19 +0300

    Final changes

--- src/Car.java
+++ src/Car.java
@@ -8,11 +8,26 @@ public class Car {
     public String countryOfOrigin;
 
     public Car(String brand, String model, double volumeOfEngine, String colour, int yearOfProduction, String countryOfOrigin) {
-        this.brand = brand;
-        this.model = model;
-        this.volumeOfEngine = volumeOfEngine;
-        this.colour = colour;
-        this.yearOfProduction = yearOfProduction;
-        this.countryOfOrigin = countryOfOrigin;
+        if(brand==null) {
+            this.brand = "default";
+        } else this.brand = brand;
+
+        if(model==null) {
+            this.model = "default";
+        } else this.model = model;
+        if(countryOfOrigin==null) {
+            this.countryOfOrigin = "default";
+        } else this.countryOfOrigin = countryOfOrigin;
+        if(Double.compare(volumeOfEngine,0)==0) {
+            this.volumeOfEngine = 1.5;
+        } else this.volumeOfEngine = volumeOfEngine;
+        if(colour==null) {
+            this.colour = "белый";
+        } else this.colour = colour;
+        if(yearOfProduction==0) {
+            this.yearOfProduction = 2000;
+        } else this.yearOfProduction = yearOfProduction;
+
+
     }
 }
